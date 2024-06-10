a2b1e6074e30c4a02696c542fc9de5d276f37dc6
brian kanari <briangitui26@gmail.com>
2024-06-10 17:33:47 +0300

    adds solution

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,17 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe () {
+    function handlefocus() {
+        console.log('Good!');
+    }
+    function handleblur() {
+        console.log('Hey! Eyes on me!');
+    }
+    return(
+        <button onFocus={handlefocus} onBlur={handleblur}>
+            Eyes on me</button>
+    )
+}
+
+export default EyesOnMe
